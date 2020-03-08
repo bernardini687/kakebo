105ef679c35f89498f40261300f61590b470378e
oscar bernardini <bernardini687@gmail.com>
2020-03-08 21:10:03 +0100

    test help messages

--- test/router.test.js
+++ test/router.test.js
@@ -1,14 +1,12 @@
 /* eslint-env mocha */
 
 const assert = require('assert')
+const man = require('../lib/help')
 const parseArgv = require('minimist')
 const router = require('../lib/router')
 const sinon = require('sinon')
 
 describe('read actions', async () => {
-  // ['--meow=foo'], // calls READ usage
-  // [] // calls handleError with READ usage
-
   describe('monthly options', async () => {
     beforeEach(async () => {
       sinon.spy(router, 'readMonth')
@@ -37,6 +35,37 @@ describe('read actions', async () => {
       const readMonthSpy = router.readMonth.getCall(0)
 
       assert.strictEqual(true, readMonthSpy.calledWithExactly(-1))
+    })
+  })
+
+  describe('invalid options', async () => {
+    beforeEach(async () => {
+      sinon.stub(router, 'err').callsFake(() => null)
+    })
+
+    afterEach(async () => {
+      sinon.restore()
+    })
+
+    it('shows help when given nothing', async () => {
+      router.dispatch({ _: [] })
+      const errSpy = router.err.getCall(0)
+
+      assert.strictEqual(true, errSpy.calledWithExactly(man.readHelp))
+    })
+
+    it('shows help when given more than one option', async () => {
+      router.dispatch(parseArgv(['-1', '-2']))
+      const errSpy = router.err.getCall(0)
+
+      assert.strictEqual(true, errSpy.calledWithExactly(man.readHelp))
+    })
+
+    it('shows help when option\'s value is not numeric', async () => {
+      router.dispatch(parseArgv(['--meow=foo']))
+      const errSpy = router.err.getCall(0)
+
+      assert.strictEqual(true, errSpy.calledWithExactly(man.readHelp))
     })
   })
 })

6c02f272fb4c0ee69d477688c58b0485aec0ccba
oscar bernardini <bernardini687@gmail.com>
2020-03-08 22:00:46 +0100

    shorter it descriptions

--- test/router.test.js
+++ test/router.test.js
@@ -44,6 +44,7 @@ describe('read actions', async () => {
       assert.strictEqual(true, errSpy.calledWithExactly(man.readHelp))
     })
   })
+
   describe('daily options', async () => {
     beforeEach(async () => {
       sinon.spy(router, 'callRead')
@@ -53,20 +54,21 @@ describe('read actions', async () => {
       sinon.restore()
     })
 
-    it('calls `callRead` with `true` and `d` when given `-d`', async () => {
+    it('calls `read` with `true` and `d` when given `-d`', async () => {
       router.dispatch(parseArgv(['-d']))
       const callReadSpy = router.callRead.getCall(0)
 
       assert.strictEqual(true, callReadSpy.calledWithExactly(true, 'd'))
     })
 
-    it('calls `callRead` with `1` and `d` when given `--day 1`', async () => {
+    it('calls `read` with `1` and `d` when given `--day 1`', async () => {
       router.dispatch(parseArgv(['--day', '1']))
       const callReadSpy = router.callRead.getCall(0)
 
       assert.strictEqual(true, callReadSpy.calledWithExactly(1, 'd'))
     })
   })
+
   describe('weekly options', async () => {
     beforeEach(async () => {
       sinon.spy(router, 'callRead')
@@ -76,20 +78,21 @@ describe('read actions', async () => {
       sinon.restore()
     })
 
-    it('calls `callRead` with `true` and `w` when given `-w`', async () => {
+    it('calls `read` with `true` and `w` when given `-w`', async () => {
       router.dispatch(parseArgv(['-w']))
       const callReadSpy = router.callRead.getCall(0)
 
       assert.strictEqual(true, callReadSpy.calledWithExactly(true, 'w'))
     })
 
-    it('calls `callRead` with `1` and `w` when given `--week 1`', async () => {
+    it('calls `read` with `1` and `w` when given `--week 1`', async () => {
       router.dispatch(parseArgv(['--week', '1']))
       const callReadSpy = router.callRead.getCall(0)
 
       assert.strictEqual(true, callReadSpy.calledWithExactly(1, 'w'))
     })
   })
+
   describe('monthly options', async () => {
     beforeEach(async () => {
       sinon.spy(router, 'callRead')
@@ -99,27 +102,28 @@ describe('read actions', async () => {
       sinon.restore()
     })
 
-    it('calls `callRead` with `true` and `m` when given `-m`', async () => {
+    it('calls `read` with `true` and `m` when given `-m`', async () => {
       router.dispatch(parseArgv(['-m']))
       const callReadSpy = router.callRead.getCall(0)
 
       assert.strictEqual(true, callReadSpy.calledWithExactly(true, 'm'))
     })
 
-    it('calls `callRead` with `1` and `m` when given `--month 1`', async () => {
+    it('calls `read` with `1` and `m` when given `--month 1`', async () => {
       router.dispatch(parseArgv(['--month', '1']))
       const callReadSpy = router.callRead.getCall(0)
 
       assert.strictEqual(true, callReadSpy.calledWithExactly(1, 'm'))
     })
 
-    it('calls `callRead` with `-1` and `m` when given `--meow=-1`', async () => {
+    it('calls `read` with `-1` and `m` when given `--meow=-1`', async () => {
       router.dispatch(parseArgv(['--meow=-1']))
       const callReadSpy = router.callRead.getCall(0)
 
       assert.strictEqual(true, callReadSpy.calledWithExactly(-1, 'm'))
     })
   })
+
   describe('yearly options', async () => {
     beforeEach(async () => {
       sinon.spy(router, 'callRead')
@@ -129,14 +133,14 @@ describe('read actions', async () => {
       sinon.restore()
     })
 
-    it('calls `callRead` with `true` and `y` when given `-y`', async () => {
+    it('calls `read` with `true` and `y` when given `-y`', async () => {
       router.dispatch(parseArgv(['-y']))
       const callReadSpy = router.callRead.getCall(0)
 
       assert.strictEqual(true, callReadSpy.calledWithExactly(true, 'y'))
     })
 
-    it('calls `callRead` with `1` and `y` when given `--year 1`', async () => {
+    it('calls `read` with `1` and `y` when given `--year 1`', async () => {
       router.dispatch(parseArgv(['--year', '1']))
       const callReadSpy = router.callRead.getCall(0)
 

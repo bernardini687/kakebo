1ac13237371e3d916e3f38f5e97c14185cb21ebf
oscar bernardini <bernardini687@gmail.com>
2020-03-10 21:43:36 +0100

    notes on a bug

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const parseArgv = require('minimist')
 const router = require('./lib/router')
 
 const argv = process.argv.slice(2)
-
 console.log('[DEBUG] argv', argv)
+
 const parsed = parseArgv(argv)
 console.log('[DEBUG] parsed', parsed)
 

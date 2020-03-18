af10fadb855ccd3e24fb8a628eadda74c956acec
oscar bernardini <bernardini687@gmail.com>
2020-03-18 13:53:54 +0100

    manual test add entries with simpler store

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const parseArgv = require('minimist')
 const router = require('./lib/router')
 
 const argv = process.argv.slice(2)
-console.log('[DEBUG] argv', argv)
+console.log('[DEBUG] argv:', argv)
 
 const parsed = parseArgv(argv)
-console.log('[DEBUG] parsed', parsed)
+console.log('[DEBUG] parsed:', parsed)
 
 router.dispatch(parsed)
 

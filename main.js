ef789ce8525b3d4bfe128162b775dd0ac67b73c5
oscar bernardini <bernardini687@gmail.com>
2020-03-23 21:23:12 +0100

    remove debugger logs

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 #!/usr/bin/env node
 
-// const parsed = require('minimist')(process.argv.slice(2))
-const parseArgv = require('minimist')
+const parsed = require('minimist')(process.argv.slice(2))
 const router = require('./lib/router')
-
-const argv = process.argv.slice(2)
-const parsed = parseArgv(argv)
-console.log('[DEBUG] argv, parsed:', [argv, parsed])
-
 router.dispatch(parsed)
